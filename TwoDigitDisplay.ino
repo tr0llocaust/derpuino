4a7b8cf939e2464b1aa5bf8827a8ab0c76d49bcc
Doug Marvel <douglas.j.marvel@gmail.com>
2016-12-02 03:33:16 +0000

    Added some dirty dirty pwm for a buzzer

--- TwoDigitDisplay.ino
+++ TwoDigitDisplay.ino
@@ -1,4 +1,5 @@
 byte potPin   = 0;
+byte buzzerPin= 3;
 byte latchPin = 8;
 byte clockPin = 12;
 byte dataPin  = 11;
@@ -20,18 +21,28 @@ byte numbers[10]=
   103}; // 9  
 
 void setup(){
+  //Serial.begin(19200);
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
 }
 
 void loop(){
   potReading  = map(analogRead(0), 0, 1019, 0, 99); // Map the range of input from the pot to a percentage
   tens        = floor(potReading / 10);             // Grab the tens digit
-  ones        = potReading % 10;                    //And the ones digit
+  ones        = potReading % 10;                    // And the ones digit
 
   digitalWrite(latchPin, LOW);                          // Instruct the register to listen for data [I think]
   shiftOut(dataPin, clockPin, MSBFIRST, numbers[ones]); // Feed the right digit from the left
   shiftOut(dataPin, clockPin, MSBFIRST, numbers[tens]); // Push it over with the left digit
   digitalWrite(latchPin, HIGH);                         // Send the data to the register
+  
+  // Cheapo PWM for the peizo buzzer weeeeeeee
+  if (potReading > 0) {
+    //Serial.println(100-potReading);
+    digitalWrite(buzzerPin, HIGH);
+    delayMicroseconds((255 - map(potReading, 0, 99, 0, 255)) * 15);
+    digitalWrite(buzzerPin, LOW);
+  }
 }
